0c83800bfc66339c0beedd18541f58ace705a3ad
Poteau Florian <florianpoteau59@gmail.com>
2024-10-08 11:15:58 +0200

    feat(form): add form style

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Carousels
   const carousels = document.querySelectorAll(".carousel");
 
   carousels.forEach((carousel) => {
@@ -7,17 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const prevButton = carousel.querySelector(".prev-btn");
     let currentIndex = 0;
 
-    // Affiche la première image
     images[currentIndex].classList.add("active");
 
-    // Fonction pour montrer l'image à l'index donné
     const showImage = (index) => {
-      images[currentIndex].classList.remove("active"); // Masquer l'image actuelle
-      currentIndex = (index + images.length) % images.length; // Calculer le nouvel index
-      images[currentIndex].classList.add("active"); // Afficher la nouvelle image
+      images[currentIndex].classList.remove("active");
+      currentIndex = (index + images.length) % images.length;
+      images[currentIndex].classList.add("active");
     };
 
-    // Écouteurs d'événements pour les boutons
     nextButton.addEventListener("click", () => {
       showImage(currentIndex + 1);
     });
@@ -26,4 +24,61 @@ document.addEventListener("DOMContentLoaded", () => {
       showImage(currentIndex - 1);
     });
   });
+
+  // Input
+
+  // Sélectionne tous les inputs et textareas dans le formulaire
+  const formFields = document.querySelectorAll("input, textarea");
+
+  // Parcours chaque champ pour détecter quand l'utilisateur appuie sur 'Entrée'
+  formFields.forEach((field, index) => {
+    field.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        if (field.tagName === "TEXTAREA") {
+          // Ne rien faire ici pour permettre le retour à la ligne
+          return;
+        }
+
+        event.preventDefault(); // Empêche le comportement par défaut d'envoyer le formulaire
+
+        // Passe au champ suivant si disponible
+        const nextField = formFields[index + 1];
+        if (nextField) {
+          nextField.focus(); // Déplace le focus vers le champ suivant
+        }
+      }
+    });
+  });
+});
+
+// email
+
+import { init, send } from "emailjs-com";
+
+const YOUR_SERVICE_ID = "";
+const YOUR_TEMPLATE_ID = "";
+const YOUR_USER_ID = "YOUR_USER_ID";
+
+init(YOUR_USER_ID);
+
+const form = document.querySelector(".contact-me-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // Empêche le rechargement de la page
+
+  const formData = new FormData(form);
+  const data = {
+    name: formData.get("name"),
+    firstname: formData.get("firstname"),
+    details: formData.get("details"),
+  };
+
+  send(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, data)
+    .then((response) => {
+      console.log("SUCCESS!", response.status, response.text);
+      // Vous pouvez afficher un message de succès à l'utilisateur ici
+    })
+    .catch((err) => {
+      console.error("FAILED...", err);
+      // Vous pouvez afficher un message d'erreur à l'utilisateur ici
+    });
 });
